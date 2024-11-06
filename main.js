41bb6993b77cf7d7326358e3c8b30d305cde3b95
yana <tayoempus@gmail.com>
2024-11-06 11:16:24 +0700

    update

---
+++ main.js
@@ -0,0 +1,44 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  getDoc,
+  deleteDoc,
+  doc,
+  query,
+  orderBy,
+  updateDoc
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBm9HdJ92vSLrKRclI6Z2J4bmvlFgR4AuU",
+  authDomain: "mang-yana.firebaseapp.com",
+  projectId: "mang-yana",
+  storageBucket: "mang-yana.appspot.com",
+  messagingSenderId: "1094982396668",
+  appId: "1:1094982396668:web:2d103526ab40a59efc0579",
+  measurementId: "G-MF48P7VG5P"
+};
+
+// ambil data untuk mapel hari senin
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export async function ambilDaftarmenu() {
+  const refDokumen = collection(db, "nabawi");
+  const kueri = query(refDokumen, orderBy("nama"));
+  const cuplikanKueri = await getDocs(kueri);
+
+  let hasil = [];
+  cuplikanKueri.forEach((dok) => {
+    hasil.push({
+      id: dok.id,
+      nama: dok.data().nama,
+      harga: dok.data().harga,
+   
+    });
+  });
+  return hasil;
+}
